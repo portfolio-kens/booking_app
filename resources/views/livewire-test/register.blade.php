e7f094af7adcf81ace23f5489a28267e15bdcf1f
kenchan1979 <saimyonkk@gmail.com>
2022-04-05 03:26:16 +0900

    second

---
+++ resources/views/livewire-test/register.blade.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        @livewireStyles
+
+        <!-- Scripts -->
+        <script src="{{ mix('js/app.js') }}" defer></script>
+    </head>
+    <body>
+        livewiretest <span class="text-blue-300">register</span> 
+        {{-- <livewire:counter /> --}}
+        @livewire('register')
+        @livewireScripts
+    </body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LivewireTestController;
+// use Barryvdh\Debugbar\DataCollector\LivewireCollector;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+Route::controller(LivewireTestController::class)
+->prefix('livewire-test')->name('livewire-test.')->group(function(){
+    Route::get('index', 'index')->name('index');
+    Route::get('register', 'register')->name('register');
+
+    });
+
